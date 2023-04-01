788621fcd90544c491f58871ffcffe43604b695e
churanta <churantamondal321@gmail.com>
2023-04-01 13:55:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:service_app/Authentication/login_screen.dart';
+import 'package:service_app/Authentication/signup_screen.dart';
+import 'package:service_app/Authentication/verifyotp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/otpInput': (context) => VerifyOtpPage(),
+      },
+    );
+  }
+}
